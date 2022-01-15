3d71682eae38e49f31b39311f384bc6a37040f3f
Flandre3569 <1332726596@qq.com>
2022-01-15 23:05:08 +0800

    基础语法

---
+++ pyDemo_workspace/demo1.py
@@ -0,0 +1,17 @@
+# -*- codeing = utf-8 -*-
+# @Time: 2022/1/15 22:32
+# @Author: Coisini
+# @File: demo1.py
+# @Software: PyCharm
+
+print("hello world")
+
+# 格式化输出
+age = 18
+print("我的年龄是: %d岁" % age)
+name = 'Coisini'
+country = "中国"
+print("我的名字是: %s, 我的国籍是: %s" % (name, country))
+
+# 分割
+print("www", "baidu", "com", sep=".")
---
+++ pyDemo_workspace/输入输出练习.py
@@ -0,0 +1,19 @@
+# -*- codeing = utf-8 -*-
+# @Time: 2022/1/15 22:51
+# @Author: Coisini
+# @File: 输入输出练习.py
+# @Software: PyCharm
+
+# 从控制台输入数据
+password = input("请输入密码: ")
+print("您刚才输入的密码是:", password)
+
+a = 10
+# 判断类型
+print(type(a))
+print(type(password))
+
+
+c = int(input("输入一个数字:"))
+print("输入的数字是: %d" % c)
+
