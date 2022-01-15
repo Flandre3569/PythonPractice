21d88f3f751744db450b4d48982947a9fc28b1eb
Flandre3569 <1332726596@qq.com>
2022-01-15 23:19:40 +0800

    条件语句

---
+++ pyDemo_workspace/条件语句.py
@@ -0,0 +1,18 @@
+# -*- codeing = utf-8 -*-
+# @Time: 2022/1/15 23:08
+# @Author: Coisini
+# @File: 条件语句.py
+# @Software: PyCharm
+
+# if的基本句式
+score = int(input("请输入成绩: "))
+
+if score >= 90:
+    print("优秀")
+elif score >= 60:
+    print("及格")
+else:
+    print("不合格")
+
+
+
