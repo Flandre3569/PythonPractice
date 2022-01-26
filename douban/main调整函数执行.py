231601b44ad40a2308c265745695a709737b4af3
Flandre3569 <1332726596@qq.com>
2022-01-26 23:31:25 +0800

    main调整函数执行顺序

---
+++ douban/main调整函数执行.py
@@ -0,0 +1,20 @@
+# -*- codeing = utf-8 -*-
+# @Time: 2022/1/26 23:23
+# @Author: Coisini
+# @File: main调整函数执行.py
+# @Software: PyCharm
+
+def main():
+    print("hello")
+
+def begin():
+    print("code begin")
+
+def end():
+    print("code end")
+
+
+if __name__ == "__main__":
+    begin()
+    main()
+    end()
